2873dc42e92c17e34b3bb92410835709a2511173
MariaEduardaTeixeira <mariaeduardat.silvano@gmail.com>
2023-05-15 20:10:42 -0300

    Retirei o comentario de alguns atributos

--- src/model/Hospedagem.java
+++ src/model/Hospedagem.java
@@ -8,11 +8,11 @@ public class Hospedagem {
     private Integer codigo;
     private LocalDate checkin;
     private LocalDate checkout;
-   // private Hospede hospede;
+    private Hospede hospede;
     private Integer quantidadeDiarias;
     private Consumo consumo;
     private Double valorTotal;
     private Double valorDiaria;
-  //  private Pagamento pagamento;
-   // private Quarto quarto;
+    private Pagamento pagamento;
+    private Quarto quarto;
 }
