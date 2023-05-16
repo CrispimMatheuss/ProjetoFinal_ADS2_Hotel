1612f66792477c79fa48e0324e575d76b7bdf0cf
MariaEduardaTeixeira <mariaeduardat.silvano@gmail.com>
2023-05-15 21:07:55 -0300

    Criação dos construtores, getters, toString e criação da Lista de Serviço no ServicoDao no repository com os métodos salvar, excluir e buscaTodos

--- src/model/Consumo.java
+++ src/model/Consumo.java
@@ -7,4 +7,43 @@ public class Consumo {
     private Hospedagem hospedagem;
     private Double valorTotal;
     private LocalDate data;
+
+    public Consumo(Integer codigo, Servico servico, Hospedagem hospedagem, Double valorTotal, LocalDate data) {
+        this.codigo = codigo;
+        this.servico = servico;
+        this.hospedagem = hospedagem;
+        this.valorTotal = valorTotal;
+        this.data = data;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public Servico getServico() {
+        return servico;
+    }
+
+    public Hospedagem getHospedagem() {
+        return hospedagem;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return "Consumo{" +
+                "codigo=" + codigo +
+                ", servico=" + servico +
+                ", hospedagem=" + hospedagem +
+                ", valorTotal=" + valorTotal +
+                ", data=" + data +
+                '}';
+    }
 }
