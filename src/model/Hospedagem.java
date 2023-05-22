d4cfa3bd73ee522becf7ceaec300007337163289
dinobgava <dinobarbettagava@gmail.com>
2023-05-22 20:37:23 -0300

    commit hospedagem

--- src/model/Hospedagem.java
+++ src/model/Hospedagem.java
@@ -1,8 +1,9 @@
 package model;
 
-import model.Consumo;
-
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hospedagem {
     private Integer codigo;
@@ -10,89 +11,10 @@ public class Hospedagem {
     private LocalDate checkout;
     private Hospede hospede;
     private Integer quantidadeDiarias;
-    private Consumo consumo;
-    private Double valorTotal;
-    private Double valorDiaria;
+    private List<Servico> consumo = new ArrayList<>();
+    private BigDecimal valorTotal;
     private Pagamento pagamento;
     private Quarto quarto;
 
-    public Integer getCodigo() {
-        return codigo;
-    }
 
-    public void setCodigo(Integer codigo) {
-        this.codigo = codigo;
-    }
-
-    public LocalDate getCheckin() {
-        return checkin;
-    }
-
-    public void setCheckin(LocalDate checkin) {
-        this.checkin = checkin;
-    }
-
-    public LocalDate getCheckout() {
-        return checkout;
-    }
-
-    public void setCheckout(LocalDate checkout) {
-        this.checkout = checkout;
-    }
-
-    public Hospede getHospede() {
-        return hospede;
-    }
-
-    public void setHospede(Hospede hospede) {
-        this.hospede = hospede;
-    }
-
-    public Integer getQuantidadeDiarias() {
-        return quantidadeDiarias;
-    }
-
-    public void setQuantidadeDiarias(Integer quantidadeDiarias) {
-        this.quantidadeDiarias = quantidadeDiarias;
-    }
-
-    public Consumo getConsumo() {
-        return consumo;
-    }
-
-    public void setConsumo(Consumo consumo) {
-        this.consumo = consumo;
-    }
-
-    public Double getValorTotal() {
-        return valorTotal;
-    }
-
-    public void setValorTotal(Double valorTotal) {
-        this.valorTotal = valorTotal;
-    }
-
-    public Double getValorDiaria() {
-        return valorDiaria;
-    }
-
-    public void setValorDiaria(Double valorDiaria) {
-        this.valorDiaria = valorDiaria;
-    }
-
-    public Pagamento getPagamento() {
-        return pagamento;
-    }
-
-    public void setPagamento(Pagamento pagamento) {
-        this.pagamento = pagamento;
-    }
-
-    public Quarto getQuarto() {
-        return quarto;
-    }
-
-    public void setQuarto(Quarto quarto) {
-        this.quarto = quarto;
-    }
 }
