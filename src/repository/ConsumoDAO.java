2f61934fa1132f63d9ea0ef33fda81abcf647e4e
ramonlindao <ramon_joaquim1@hotmail.com>
2023-05-22 20:02:45 -0300

    Atualizei o ConsumoDAO pois estava dando conflito e arrumei ele

--- src/repository/ConsumoDAO.java
+++ src/repository/ConsumoDAO.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-package repository;
-
-import model.Servico;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ConsumoDAO {
-    static List<Servico> servicos = new ArrayList<>();
-
-    public static void adicionarServico(Servico servico) {
-        servicos.add(servico);
-    }
-
-    public void excluir(Servico servico) {
-        servicos.remove(servico);
-    }
-
-    public static List<Servico> buscaTodos() {
-        return servicos;
-    }
-
-    public double calcularCustoTotal() {
-        double custoTotal = 0.0;
-        for (Servico servico : servicos) {
-            custoTotal += servico.calcularCustoServicos();
-        }
-        return custoTotal;
-    }
-
-
-}
-=======
 package repository;
 
 import model.Servico;
@@ -64,4 +30,3 @@ public class ConsumoDAO {
 
 
 }
->>>>>>> origin/master
