b34e0da42614000b371e7f1632508eb4e011db84
dinobgava <dinobarbettagava@gmail.com>
2023-06-15 17:13:21 -0300

    Alterações no main - ajustes na hospedagem metodo chama checkout, agora inserindo data de saida e quantidade de diarias na classe hospedagem

--- src/repository/HospedagemDAO.java
+++ src/repository/HospedagemDAO.java
@@ -23,29 +23,29 @@ public class HospedagemDAO {
 
     public static List<Hospedagem> buscaTodos() {
         if (hospedagens.isEmpty()) {
-            hospedagens.add(new Hospedagem(10, LocalDate.now(), LocalDate.now(),
-                    new Hospede("premium", "5664545", "j54845456445", "joao da silva hotimaiu"),
+            hospedagens.add(new Hospedagem(10, LocalDate.of(2023,05,15), LocalDate.now(),
+                    new Hospede("joao", "5664545", "j54845456445", "joao da silva hotimaiu"),
                     3, new Quarto(1, TipoQuarto.PADRAO_CLASSICO, BigDecimal.valueOf(100.00), "107",
                     DisponibilidadeQuarto.DISPONIVEL, 3)));
 
-            hospedagens.add(new Hospedagem(20, LocalDate.now(), LocalDate.now(),
-                    new Hospede("standard", "5664545", "j54845456445", "pedro da silva hotimaiu"),
-                    4, new Quarto(1, TipoQuarto.PADRAO_CLASSICO, BigDecimal.valueOf(200.00), "108",
+            hospedagens.add(new Hospedagem(20, LocalDate.of(2023,05,15), LocalDate.now(),
+                    new Hospede("pedro", "5664545", "j54845456445", "pedro da silva hotimaiu"),
+                    4, new Quarto(2, TipoQuarto.PADRAO_CLASSICO, BigDecimal.valueOf(200.00), "108",
                     DisponibilidadeQuarto.DISPONIVEL, 2)));
 
-            hospedagens.add(new Hospedagem(30, LocalDate.now(), LocalDate.now(),
-                    new Hospede("master", "5664545", "j54845456445", "maria da silva hotimaiu"),
-                    2, new Quarto(1, TipoQuarto.MASTER_VIP, BigDecimal.valueOf(300.00), "109",
+            hospedagens.add(new Hospedagem(30, LocalDate.of(2023,05,15), LocalDate.now(),
+                    new Hospede("maria", "5664545", "j54845456445", "maria da silva hotimaiu"),
+                    2, new Quarto(3, TipoQuarto.MASTER_VIP, BigDecimal.valueOf(300.00), "109",
                     DisponibilidadeQuarto.DISPONIVEL, 6)));
 
-            hospedagens.add(new Hospedagem(40, LocalDate.now(), LocalDate.now(),
-                    new Hospede("vip", "5664545", "j54845456445", "jose da silva hotimaiu"),
-                    3, new Quarto(1, TipoQuarto.VIP, BigDecimal.valueOf(450.00), "110",
+            hospedagens.add(new Hospedagem(40, LocalDate.of(2023,05,15), LocalDate.now(),
+                    new Hospede("jose", "5664545", "j54845456445", "jose da silva hotimaiu"),
+                    3, new Quarto(4, TipoQuarto.VIP, BigDecimal.valueOf(450.00), "110",
                     DisponibilidadeQuarto.DISPONIVEL, 3)));
 
-            hospedagens.add(new Hospedagem(50, LocalDate.now(), LocalDate.now(),
-                    new Hospede("premium", "5664545", "j54845456445", "murilo da silva hotimaiu"),
-                    5, new Quarto(1, TipoQuarto.STAR_VIP, BigDecimal.valueOf(500.00), "120",
+            hospedagens.add(new Hospedagem(50, LocalDate.of(2023,05,15), LocalDate.now(),
+                    new Hospede("murilo", "5664545", "j54845456445", "murilo da silva hotimaiu"),
+                    5, new Quarto(5, TipoQuarto.STAR_VIP, BigDecimal.valueOf(500.00), "120",
                     DisponibilidadeQuarto.DISPONIVEL, 3)));
         }
         return hospedagens;
